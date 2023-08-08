3f93cc29bbe90dc1442c8024a58035e31e8ffb71
Shravan Manoj <57617166+shravan9112@users.noreply.github.com>
2023-08-08 03:15:42 +0100

    UI first version

--- My Little Drift Car/Assets/Scripts/Gamemanager.cs
+++ My Little Drift Car/Assets/Scripts/Gamemanager.cs
@@ -5,26 +5,66 @@ using Photon.Pun;
 
 public class Gamemanager : MonoBehaviour
 {
-    public GameObject s14drift,s2kdrift,ui;
+    private int selector,final;
+    public GameObject first,second;
+    public GameObject[] cars;
     public Vector3 pos = new Vector3(330.2f,21.48f,65f);
     public bool photon;
 
-    public void s14d()
+    public void s14()
     {
-        Debug.Log("S14 Drift Pressed");
-        ui.SetActive(false);
-        if (photon)
-            PhotonNetwork.Instantiate(s14drift.name, pos, Quaternion.identity);
-        else
-            Instantiate(s14drift,pos, Quaternion.identity);
+        selector = 1;
+        Debug.Log("S14 Pressed");
+        first.SetActive(false);
+        second.SetActive(true);
+        //if (photon)
+        //    PhotonNetwork.Instantiate(s14drift.name, pos, Quaternion.identity);
+        //else
+        //    Instantiate(s14drift,pos, Quaternion.identity);
     }
-    public void s2kd()
+    public void s2k()
     {
-        Debug.Log("S2K Drift Pressed");
-        ui.SetActive(false);
-        if (photon)
-            PhotonNetwork.Instantiate(s2kdrift.name, pos, Quaternion.identity);
-        else
-            Instantiate(s2kdrift);
+        selector = 2;
+        Debug.Log("S2K Pressed");
+        first.SetActive(false);
+        second.SetActive(true);
+        //if (photon)
+        //    PhotonNetwork.Instantiate(s2kdrift.name, pos, Quaternion.identity);
+        //else
+        //    Instantiate(s2kdrift);
+    }
+
+    public void wrx()
+    {
+        selector = 3;
+        Debug.Log("WRX Pressed");
+        first.SetActive(false);
+        second.SetActive(true);
+    }
+
+    public void r34()
+    {
+        selector = 4;
+        Debug.Log("R34 Pressed");
+        first.SetActive(false);
+        second.SetActive(true);
+    }
+
+    public void back()
+    {
+        selector = 0;
+        Debug.Log("Back pressed");
+        second.SetActive(false);
+        first.SetActive(true);
+    }
+
+    public void race()
+    {
+        final = selector + 4;
+    }
+
+    public void drift()
+    {
+        final = selector;
     }
 }
