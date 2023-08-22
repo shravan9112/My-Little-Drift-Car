89c8214288078580a10a26dcd9c5e9585b2e24a2
Shravan Manoj <57617166+shravan9112@users.noreply.github.com>
2023-08-22 21:28:48 +0100

    Anti Roll update

--- My Little Drift Car/Assets/Scripts/Test/AntiRoll.cs
+++ My Little Drift Car/Assets/Scripts/Test/AntiRoll.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class AntiRoll : MonoBehaviour
 {
-    [SerializeField] Wheel FrontLeftWheel;
-    [SerializeField] Wheel FrontRightWheel;
+    //[SerializeField] Wheel FrontLeftWheel;
+    //[SerializeField] Wheel FrontRightWheel;
     [SerializeField] Wheel RearLeftWheel;
     [SerializeField] Wheel RearRightWheel;
     [SerializeField] float roll = 5000.0f;
@@ -22,13 +22,13 @@ public class AntiRoll : MonoBehaviour
         var travelL = 1.0;
         var travelR = 1.0;
 
-        var groundedFL = FrontLeftWheel.WheelCollider.GetGroundHit(out hit);
-        if (groundedFL)
-            travelL = (-FrontLeftWheel.WheelCollider.transform.InverseTransformPoint(hit.point).y - FrontLeftWheel.WheelCollider.radius) / FrontLeftWheel.WheelCollider.suspensionDistance;
+        //var groundedFL = FrontLeftWheel.WheelCollider.GetGroundHit(out hit);
+        //if (groundedFL)
+        //    travelL = (-FrontLeftWheel.WheelCollider.transform.InverseTransformPoint(hit.point).y - FrontLeftWheel.WheelCollider.radius) / FrontLeftWheel.WheelCollider.suspensionDistance;
 
-        var groundedFR = FrontRightWheel.WheelCollider.GetGroundHit(out hit);
-        if (groundedFR)
-            travelR = (-FrontRightWheel.WheelCollider.transform.InverseTransformPoint(hit.point).y - FrontRightWheel.WheelCollider.radius) / FrontRightWheel.WheelCollider.suspensionDistance;
+        //var groundedFR = FrontRightWheel.WheelCollider.GetGroundHit(out hit);
+        //if (groundedFR)
+        //    travelR = (-FrontRightWheel.WheelCollider.transform.InverseTransformPoint(hit.point).y - FrontRightWheel.WheelCollider.radius) / FrontRightWheel.WheelCollider.suspensionDistance;
 
         var groundedRL = RearLeftWheel.WheelCollider.GetGroundHit(out hit);
         if (groundedRL)
@@ -41,17 +41,17 @@ public class AntiRoll : MonoBehaviour
 
         var antiRollForce = (travelL - travelR) * roll;
 
-        if (groundedFL)
-            rB.AddForceAtPosition(FrontLeftWheel.WheelCollider.transform.up * -roll,
-                   FrontLeftWheel.WheelCollider.transform.position);
-        if (groundedFR)
-            rB.AddForceAtPosition(FrontRightWheel.WheelCollider.transform.up * roll,
-                   FrontRightWheel.WheelCollider.transform.position);
+        //if (groundedFL)
+        //    rB.AddForceAtPosition(FrontLeftWheel.WheelCollider.transform.up * -roll,
+        //           FrontLeftWheel.WheelCollider.transform.position);
+        //if (groundedFR)
+        //    rB.AddForceAtPosition(FrontRightWheel.WheelCollider.transform.up * roll,
+        //           FrontRightWheel.WheelCollider.transform.position);
         if (groundedRL)
             rB.AddForceAtPosition(RearLeftWheel.WheelCollider.transform.up * -roll,
                    RearLeftWheel.WheelCollider.transform.position);
         if (groundedRR)
-            rB.AddForceAtPosition(RearRightWheel.WheelCollider.transform.up * -roll,
+            rB.AddForceAtPosition(RearRightWheel.WheelCollider.transform.up * roll,
                    RearRightWheel.WheelCollider.transform.position);
 
     }
